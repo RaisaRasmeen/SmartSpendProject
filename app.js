00747d20e8ff538331d2f06f92722a0a702d1e56
RaisaRasmeen <raisarasmeen01234@gmail.com>
2026-08-14 23:49:24 +0600

    12th

--- app.js
+++ app.js
@@ -143,3 +143,14 @@ function drawChart() {
         </div>`;
     }).join('');
 }
+function formatDate(iso) {
+  try {
+    return new Date(iso).toLocaleDateString('en-GB', {
+      day: '2-digit',
+      month: 'short',
+      year: 'numeric'
+    });
+  } catch {
+    return iso;
+  }
+}
